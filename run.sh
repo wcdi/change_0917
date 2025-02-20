308789cf480b3da5cdfa504f12690ca0ac5a6460
MultiGamer8853 <qq3082499405@icloud.com>
2025-02-21 01:46:55 +0900

    add debian

--- run.sh
+++ run.sh
@@ -16,6 +16,16 @@ ubuntu(){
   sudo apt-get update
 }
 
+debian(){
+  APT="/etc/apt"
+  source_file="${APT}/sources.list"
+  if [ -f $source_file ]; then
+    sudo cp $source_file $source_file.bk
+    sudo sed -i 's-ht.*//.*/-http://mirror.hashy0917.net/debian/-' $source_file
+    sudo apt-get update
+  fi
+}
+
 openwrt(){
   source_file="/etc/opkg/distfeeds.conf"
   sudo cp $source_file $source_file.bk
@@ -29,6 +39,12 @@ if [ -f /etc/os-release ]; then
   case "$ID" in
     ubuntu)
       ubuntu
+      ;;
+    debian)
+      debian
+      ;;
     openwrt)
       openwrt
+      ;;
+  esac
 fi
