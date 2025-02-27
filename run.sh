14217ef3ccff0642749366b72d1057db76be72df
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-02-27 18:38:59 +0900

    a-z順に変更

--- run.sh
+++ run.sh
@@ -11,19 +11,14 @@ arch(){
   pacman -Syyu
 }
 
-ubuntu(){
+debian(){
   APT="/etc/apt"
-  source_file=""
-  # 24.04以降ファイルの位置が変わった
-  if [ -f $APT/sources.list.d/ubuntu.sources ]; then
-    source_file="${APT}/sources.list.d/ubuntu.sources"
-    sudo cp $source_file ${APT}/ubuntu.sources.bk
-  else
-    source_file="${APT}/sources.list"
+  source_file="${APT}/sources.list"
+  if [ -f $source_file ]; then
     sudo cp $source_file $source_file.bk
+    sudo sed -i 's-ht.*//.*/-http://mirror.hashy0917.net/debian/-' $source_file
+    sudo apt-get update
   fi
-  sudo sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
-  sudo apt-get update
 }
 
 kali(){
@@ -40,21 +35,26 @@ kali(){
   sudo apt-get update
 }
 
-debian(){
-  APT="/etc/apt"
-  source_file="${APT}/sources.list"
-  if [ -f $source_file ]; then
-    sudo cp $source_file $source_file.bk
-    sudo sed -i 's-ht.*//.*/-http://mirror.hashy0917.net/debian/-' $source_file
-    sudo apt-get update
-  fi
-}
-
 openwrt(){
   source_file="/etc/opkg/distfeeds.conf"
   sudo cp $source_file $source_file.bk
   sudo sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/openwrt/-' $source_file
   sudo opkg update
+}
+
+ubuntu(){
+  APT="/etc/apt"
+  source_file=""
+  # 24.04以降ファイルの位置が変わった
+  if [ -f $APT/sources.list.d/ubuntu.sources ]; then
+    source_file="${APT}/sources.list.d/ubuntu.sources"
+    sudo cp $source_file ${APT}/ubuntu.sources.bk
+  else
+    source_file="${APT}/sources.list"
+    sudo cp $source_file $source_file.bk
+  fi
+  sudo sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
+  sudo apt-get update
 }
 
 # ディストリビューションのバージョン取得
@@ -64,17 +64,17 @@ if [ -f /etc/os-release ]; then
     arch)
       arch
       ;;
-    ubuntu)
-      ubuntu
-      ;;
     debian)
       debian
+      ;;
+    kali)
+      kali
       ;;
     openwrt)
       openwrt
       ;;
-    kali)
-      kali
+    ubuntu)
+      ubuntu
       ;;
   esac
 fi
