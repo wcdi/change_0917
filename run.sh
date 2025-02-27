33b577434f23d5e6a1c21b9a1ef03fd215e51231
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-02-27 18:54:16 +0900

    Archに対応 (#19)
    * add arch
    
    * a-z順に変更
    
    * debian修正
    
    * testsを追加

--- run.sh
+++ run.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 set -eu
 
-ubuntu(){
+arch(){
+  source="/etc/pacman.d/mirrorlist"
+  if [ -f $source ]; then
+    cp $source ${source}.bk
+    echo 'Server = https://mirror.hashy0917.net/archlinux/$repo/os/$arch' > $source
+    cat $source.bk >> $source
+  fi
+  pacman -Syyu
+}
+
+debian(){
+  # APT="/etc/apt"
+  # source_file="${APT}/sources.list"
+  # if [ -f $source_file ]; then
+  #   sudo cp $source_file $source_file.bk
+  #   sudo sed -i 's-ht.*//.*/-http://mirror.hashy0917.net/debian/-' $source_file
+  #   sudo apt-get update
+  # fi
   APT="/etc/apt"
   source_file=""
   # 24.04以降ファイルの位置が変わった
-  if [ -f $APT/sources.list.d/ubuntu.sources ]; then
-    source_file="${APT}/sources.list.d/ubuntu.sources"
-    sudo cp $source_file ${APT}/ubuntu.sources.bk
+  if [ -f $APT/sources.list.d/debian.sources ]; then
+    source_file="${APT}/sources.list.d/debian.sources"
+    sudo cp $source_file ${APT}/debian.sources.bk
   else
     source_file="${APT}/sources.list"
     sudo cp $source_file $source_file.bk
@@ -30,16 +47,6 @@ kali(){
   sudo apt-get update
 }
 
-debian(){
-  APT="/etc/apt"
-  source_file="${APT}/sources.list"
-  if [ -f $source_file ]; then
-    sudo cp $source_file $source_file.bk
-    sudo sed -i 's-ht.*//.*/-http://mirror.hashy0917.net/debian/-' $source_file
-    sudo apt-get update
-  fi
-}
-
 openwrt(){
   source_file="/etc/opkg/distfeeds.conf"
   sudo cp $source_file $source_file.bk
@@ -47,21 +54,39 @@ openwrt(){
   sudo opkg update
 }
 
+ubuntu(){
+  APT="/etc/apt"
+  source_file=""
+  # 24.04以降ファイルの位置が変わった
+  if [ -f $APT/sources.list.d/ubuntu.sources ]; then
+    source_file="${APT}/sources.list.d/ubuntu.sources"
+    sudo cp $source_file ${APT}/ubuntu.sources.bk
+  else
+    source_file="${APT}/sources.list"
+    sudo cp $source_file $source_file.bk
+  fi
+  sudo sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
+  sudo apt-get update
+}
+
 # ディストリビューションのバージョン取得
 if [ -f /etc/os-release ]; then
   . /etc/os-release #source /etc/os-release
   case "$ID" in
-    ubuntu)
-      ubuntu
+    arch)
+      arch
       ;;
     debian)
       debian
       ;;
+    kali)
+      kali
+      ;;
     openwrt)
       openwrt
       ;;
-    kali)
-      kali
+    ubuntu)
+      ubuntu
       ;;
   esac
 fi
