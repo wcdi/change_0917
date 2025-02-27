a42e409f39fe823c57646b3c4e771c21aac9b813
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-02-27 18:40:36 +0900

    debian修正

--- run.sh
+++ run.sh
@@ -12,13 +12,25 @@ arch(){
 }
 
 debian(){
+  # APT="/etc/apt"
+  # source_file="${APT}/sources.list"
+  # if [ -f $source_file ]; then
+  #   sudo cp $source_file $source_file.bk
+  #   sudo sed -i 's-ht.*//.*/-http://mirror.hashy0917.net/debian/-' $source_file
+  #   sudo apt-get update
+  # fi
   APT="/etc/apt"
-  source_file="${APT}/sources.list"
-  if [ -f $source_file ]; then
+  source_file=""
+  # 24.04以降ファイルの位置が変わった
+  if [ -f $APT/sources.list.d/debian.sources ]; then
+    source_file="${APT}/sources.list.d/debian.sources"
+    sudo cp $source_file ${APT}/debian.sources.bk
+  else
+    source_file="${APT}/sources.list"
     sudo cp $source_file $source_file.bk
-    sudo sed -i 's-ht.*//.*/-http://mirror.hashy0917.net/debian/-' $source_file
-    sudo apt-get update
   fi
+  sudo sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
+  sudo apt-get update
 }
 
 kali(){
