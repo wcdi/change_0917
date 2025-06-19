e02065cb5bf7885c766c038050657b833c949560
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-06-19 17:42:10 +0900

    unix風な命名にした

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 set -eu
 
-checkfiles() {
+chkfs() {
   # Detect source_file
-  if ! $sudo test -e $source_file ; then
-    echo "$source_file is not found"
+  if ! $sudo test -e $srcpath ; then
+    echo "$srcpath is not found"
     exit 1
   fi
 
   # Detect "mirror.hashy0917.net" domain from $source_file 
-  if $sudo grep "mirror.hashy0917.net" $source_file >/dev/null 2>&1 ; then
-    echo "Already changed: Detected “mirror.hashy0917.net” domain in $source_file"
+  if $sudo grep "mirror.hashy0917.net" $srcpath >/dev/null 2>&1 ; then
+    echo "Already changed: Detected “mirror.hashy0917.net” domain in $srcpath"
     exit 1
   fi
 
-  if $sudo test -e $backup_file ; then
+  if $sudo test -e $bkpath ; then
     # backup exists
-    echo "Backup failed: $backup_file is already."
+    echo "Backup failed: $bkpath is already."
     exit 1  
   fi
 }
 
 debian() {
-  cat $source_file | sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' > $tmp_file
+  cat $srcpath | sed 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' > $tmpfile
 }
 
 # command
@@ -31,11 +31,11 @@ diff=$(if ! command -v diff 2>/dev/null ; then print 'cat'; fi)
 pkgmgr=""
 churl=""
 # repo_list
-source_file=""
-backup_file=""
+srcpath=""
+bkpath=""
 # tmp
-tmp_file=$(mktemp)
-trap 'rm -f "$tmp_file"' EXIT
+tmpfile=$(mktemp)
+trap 'rm -f "$tmpfile"' EXIT
 
 # Abort if not root and sudo is unavailable
 if test -z $sudo ; then
@@ -50,15 +50,15 @@ if [ -f /etc/os-release ]; then
   . /etc/os-release
   case "$ID" in
      ubuntu)
-      source_file="/etc/apt/sources.list"
-      backup_file="/etc/apt/sources.list.bk"
+      srcpath="/etc/apt/sources.list"
+      bkpath="/etc/apt/sources.list.bk"
       pkgmgr="apt-get update"
       churl="debian"
 
-      if $sudo test -e $source_file ; then
+      if $sudo test -e $srcpath ; then
         # after 24.04
-        source_file="/etc/apt/sources.list.d/ubuntu.sources"
-        backup_file="/etc/apt/ubuntu.sources.bk"
+        srcpath="/etc/apt/sources.list.d/ubuntu.sources"
+        bkpath="/etc/apt/ubuntu.sources.bk"
       fi
       ;;
     *)
@@ -69,20 +69,20 @@ if [ -f /etc/os-release ]; then
 fi
 
 # check files
-checkfiles
+chkfs
 
 # make backup
-$sudo cp $source_file $source_file.bk
+$sudo cp $srcpath $srcpath.bk
 
 # change repository
 $churl
 
 # show diff
-$sudo $diff $source_file $tmp_file
+$sudo $diff $srcpath $tmpfile
 echo 'Apply the changes? [confirm]'
 read
 
 # update command
-rm $source_file
-cp $tmp_file $source_file
+rm $srcpath
+cp $tmpfile $srcpath
 $sudo $pkgmgr
