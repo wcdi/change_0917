2d16f659ea1736ea04b7708c3eee07661e5454ff
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-06-19 17:02:36 +0900

    source_fileを探すように & tmp生成

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 set -eu
 
+# command
 sudo="sudo"
+diff="diff"
+pkg_mgr=""
+# repo_list
+source_file=""
+backup_file=""
+# tmp
+tmp_file=$(mktemp)
+
+# clean tmpfile
+trap 'rm -f "$tmp_file"' EXIT
 
 # Abort if not root and sudo is unavailable
 if ! command -v sudo >/dev/null 2>&1 ; then
@@ -10,6 +21,11 @@ if ! command -v sudo >/dev/null 2>&1 ; then
     echo "This script must be run as root."
     exit 1
   fi
+fi
+
+# Abort if diff is unavailable
+if ! command -v diff >/dev/null 2>&1 ; then
+  diff="cat"
 fi
 
 # Get distribution
@@ -49,134 +65,42 @@ arch(){
   # path
   source_file="/etc/pacman.d/mirrorlist"
   backup_file="$source_file.bk"
-  command='echo "Please add -y option when running pacman next time. (for example: pacman -Syu)"'
+  pkg_mgr='echo "Please add -y option when running pacman next time. (for example: pacman -Syu)"'
 
+  # Detect source_file
+  if ! $sudo test -e $source_file ; then
+    echo "$source_file is not found"
+    exit 1
+  fi
+
+  # Detect "mirror.hashy0917.net" domain from $source_file 
+  if $sudo grep "mirror.hashy0917.net" $source_file >/dev/null 2>&1 ; then
+    echo "Already changed: Detected “mirror.hashy0917.net” domain in $source_file"
+    exit 1
+  fi
+  
   # make backup
-  if [ -e $backup_file ]; then
+  if $sudo test -e $backup_file ; then
     # backup exists
     echo "Backup failed: $backup_file is already."
     exit 1  
   fi
-  cp $source_file $source_file.bk
+  $sudo cp $source_file $source_file.bk
 
   # change repository
-  if [ -f $source_file ]; then
-    echo 'Server = https://mirror.hashy0917.net/archlinux/$repo/os/$arch' > $source_file
-    cat $backup_file >> $source_file
-  fi
+  echo 'Server = https://mirror.hashy0917.net/archlinux/$repo/os/$arch' > $tmp_file
+  $sudo cat $source_file >> $tmp_file
 }
 
 debian(){
-  # path
-  source_file="/etc/apt/sources.list"
-  backup_file="/etc/apt/sources.list.bk"
-  command="apt-get update"
-  if [ -e $source_file ]; then
-    # after 24.04
-    source_file="/etc/apt/sources.list.d/debian.sources"
-    backup_file="/etc/apt/debian.sources.bk"
-  fi
-  
-  # make backup
-  if [ -e $backup_file ]; then
-    # backup exists
-    echo "Backup failed: $backup_file is already."
-    exit 1  
-  fi
-  cp $source_file $source_file.bk
-
-  # change repository
-  sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
-
-  # update command
-  $command
-}
-
-kali(){
-  # path
-  source_file="/etc/apt/sources.list"
-  backup_file="/etc/apt/sources.list.bk"
-  command="apt-get update"
-  if [ -e $source_file ]; then
-    # after 24.04
-    source_file="/etc/apt/sources.list.d/kali.sources"
-    backup_file="/etc/apt/kali.sources.bk"
-  fi
-  
-  # make backup
-  if [ -e $backup_file ]; then
-    # backup exists
-    echo "Backup failed: $backup_file is already."
-    exit 1  
-  fi
-  cp $source_file $source_file.bk
-
-  # change repository
-  sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
-
-  # update command
-  $command
-}
-
-openwrt(){
-  # path
-  source_file="/etc/opkg/distfeeds.conf"
-  backup_file="$source_file.bk"
-  command="opkg update"
-  if [ -d /etc/apk ]; then
-    # Detect apk-based OpenWrt 
-    source_file="/etc/apk/repositories.d/distfeeds.list"
-    backup_file="$source_file.bk"
-    command="apk update"
-  fi
-
-  # make backup
-  if [ -e $source_file.bk ]; then
-    # backup exists
-    echo "Backup failed: $backup_file is already."
-    exit 1  
-  fi
-  cp $source_file $source_file.bk
-
-  # change repository
-  sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/openwrt/-' $source_file
-    
-  # update command
-  $command
-}
-
-parrot(){
-  # path
-  source_file="/etc/apt/sources.list"
-  backup_file="/etc/apt/sources.list.bk"
-  command="apt-get update"
-  
-  # make backup
-  if [ -e $backup_file ]; then
-    # backup exists
-    echo "Backup failed: $backup_file is already."
-    exit 1  
-  fi
-  cp $source_file $source_file.bk
-
-  # change repository
-  # (Preserve paths that include 'direct'.)
-  sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
-  sed -i 's-http://mirror.hashy0917.net/direct/parrot-https://deb.parrot.sh/direct/parrot-' $source_file
-
-  # update command
-  $command
-}
-
-ubuntu(){
   # set path
   source_file="/etc/apt/sources.list"
   backup_file="/etc/apt/sources.list.bk"
-  command="apt-get update"
+  pkg_mgr="apt-get update"
   if $sudo test -e $source_file ; then
     # after 24.04
-    source_file="/etc/apt/sources.list.d/ubuntu.sources"
-    backup_file="/etc/apt/ubuntu.sources.bk"
+    source_file="/etc/apt/sources.list.d/debian.sources"
+    backup_file="/etc/apt/debian.sources.bk"
   fi
 
   # Detect "mirror.hashy0917.net" domain from $source_file 
@@ -197,6 +121,132 @@ ubuntu(){
   $sudo sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
 
   # update command
-  $sudo $command
+  $sudo $pkg_mgr
+}
+
+kali(){
+  # path
+  source_file="/etc/apt/sources.list"
+  backup_file="/etc/apt/sources.list.bk"
+  pkg_mgr="apt-get update"
+  if [ -e $source_file ]; then
+    # after 24.04
+    source_file="/etc/apt/sources.list.d/kali.sources"
+    backup_file="/etc/apt/kali.sources.bk"
+  fi
+  
+  # Detect "mirror.hashy0917.net" domain from $source_file 
+  if $sudo grep "mirror.hashy0917.net" $source_file >/dev/null 2>&1 ; then
+    echo "Already changed: Detected “mirror.hashy0917.net” domain in $source_file"
+    exit 1
+  fi
+  
+  # make backup
+  if $sudo test -e $backup_file ; then
+    # backup exists
+    echo "Backup failed: $backup_file is already."
+    exit 1  
+  fi
+  $sudo cp $source_file $source_file.bk
+
+  # change repository
+  $sudo sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
+
+  # update command
+  $sudo $pkg_mgr
+}
+
+openwrt(){
+  # path
+  source_file="/etc/opkg/distfeeds.conf"
+  backup_file="$source_file.bk"
+  pkg_mgr="opkg update"
+  if [ -d /etc/apk ]; then
+    # Detect apk-based OpenWrt 
+    source_file="/etc/apk/repositories.d/distfeeds.list"
+    backup_file="$source_file.bk"
+    pkg_mgr="apk update"
+  fi
+
+  # make backup
+  if [ -e $source_file.bk ]; then
+    # backup exists
+    echo "Backup failed: $backup_file is already."
+    exit 1  
+  fi
+  cp $source_file $source_file.bk
+
+  # change repository
+  sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/openwrt/-' $source_file
+    
+  # update command
+  $pkg_mgr
+}
+
+parrot(){
+  # path
+  source_file="/etc/apt/sources.list"
+  backup_file="/etc/apt/sources.list.bk"
+  pkg_mgr="apt-get update"
+  
+  # Detect "mirror.hashy0917.net" domain from $source_file 
+  if $sudo grep "mirror.hashy0917.net" $source_file >/dev/null 2>&1 ; then
+    echo "Already changed: Detected “mirror.hashy0917.net” domain in $source_file"
+    exit 1
+  fi
+  
+  # make backup
+  if $sudo test -e $backup_file ; then
+    # backup exists
+    echo "Backup failed: $backup_file is already."
+    exit 1  
+  fi
+  $sudo cp $source_file $source_file.bk
+
+  # change repository
+  # (Preserve paths that include 'direct'.)
+  $sudo sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
+  $sudo sed -i 's-http://mirror.hashy0917.net/direct/parrot-https://deb.parrot.sh/direct/parrot-' $source_file
+
+  # update command
+  $sudo $pkg_mgr
+}
+
+ubuntu(){
+  # set path
+  source_file="/etc/apt/sources.list"
+  backup_file="/etc/apt/sources.list.bk"
+  pkg_mgr="apt-get update"
+  if $sudo test -e $source_file ; then
+    # after 24.04
+    source_file="/etc/apt/sources.list.d/ubuntu.sources"
+    backup_file="/etc/apt/ubuntu.sources.bk"
+  fi
+
+  # Detect source_file
+  if ! $sudo test -e $source_file ; then
+    echo "$source_file is not found"
+    exit 1
+  fi
+
+  # Detect "mirror.hashy0917.net" domain from $source_file 
+  if $sudo grep "mirror.hashy0917.net" $source_file >/dev/null 2>&1 ; then
+    echo "Already changed: Detected “mirror.hashy0917.net” domain in $source_file"
+    exit 1
+  fi
+  
+  # make backup
+  if $sudo test -e $backup_file ; then
+    # backup exists
+    echo "Backup failed: $backup_file is already."
+    exit 1  
+  fi
+  $sudo cp $source_file $source_file.bk
+
+  # change repository
+  $sudo sed -i 's-ht.*//[A-Za-z0-9.]*/-http://mirror.hashy0917.net/-' $source_file
+
+  # update command
+  $sudo $pkg_mgr
 }
 
